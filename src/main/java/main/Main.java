11fbbb8fc8c754eccda21115f6879a6b892db124
antonio_ionut.nica <antonio_ionut.nica@stud.acs.upb.ro>
2025-11-22 21:03:12 +0200

    test field add

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,6 +3,7 @@ package main;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import fileio.InputLoader;
 
 import java.io.File;
@@ -18,6 +19,7 @@ public final class Main {
 
     private static final ObjectMapper MAPPER = new ObjectMapper();
     public static final ObjectWriter WRITER = MAPPER.writer().withDefaultPrettyPrinter();
+    public static final ObjectNode OBJECT_MAPPER = MAPPER.createObjectNode();
 
     /**
      * @param inputPath input file path
@@ -37,16 +39,22 @@ public final class Main {
          * There are multiple ways to do this, here is one example:
          *
          *
-         * ObjectNode objectNode = MAPPER.createObjectNode();
-         * objectNode.put("field_name", "field_value");
+         ObjectNode objectNode = MAPPER.createObjectNode();
+         objectNode.put("field_name", "field_value");
          *
-         * ArrayNode arrayNode = MAPPER.createArrayNode();
-         * arrayNode.add(objectNode);
+         ArrayNode arrayNode = MAPPER.createArrayNode();
+         arrayNode.add(objectNode);
          *
-         * output.add(arrayNode);
-         * output.add(objectNode);
+         output.add(arrayNode);
+         output.add(objectNode);
          *
          */
+        ObjectNode objectNode = MAPPER.createObjectNode();
+        objectNode.put("field_name", "field_value");
+        ArrayNode arrayNode = MAPPER.createArrayNode();
+        arrayNode.add(objectNode);
+        output.add(arrayNode);
+        output.add(objectNode);
 
         File outputFile = new File(outputPath);
         outputFile.getParentFile().mkdirs();
